7b79c36789e740f1e570415442468ce05b0a698c
lorenzobrusco <lorenzo.brusco@libero.it>
2017-12-12 19:08:02 +0100

    remove add post from js

--- home.js
+++ home.js
@@ -1,16 +1,6 @@
 //ignore the first 13lines,the trick here is all about tracking the element till it comes into the view, by measuring height and stuff of both window screen and the element itself, its that simple, muhammad khairy
 $(function () {
-$('.insta').clone().appendTo('.a1');
-    $('.a1:first').find('.post img').attr('src','https://goo.gl/pnYKzy')
-    $('.a1:first').find('.caption span').text('Khitma partner');
-    $('.a1:eq(1)').find('.post img').attr('src','https://goo.gl/ebVC6W')
-    $('.a1:eq(1)').find('.caption span').text('Sip your coffee quite silently Get lost into some appealing books pages');
-    $('.a1:eq(2)').find('.post img').attr('src','https://goo.gl/CtfVAj')
-    $('.a1:eq(2)').find('.caption span').text('Cupcakes can perform rites of pilgrimage too ');
-    $('.a1:eq(3)').find('.post img').attr('src','https://goo.gl/Ch5atv')
-    $('.a1:eq(3)').find('.caption span').text('Rest in Peace Ya Narges');
-    $('.a1:eq(4)').find('.post img').attr('src','https://goo.gl/ZpfGSf');
-    $('.a1:eq(4)').find('.caption span').text('I promised I will never compose poems on your eyes .. but I did');
+
 $(window).on('scroll resize', function holy_nob() {
     var    wh = $(window).height();
     var    wt = $(window).scrollTop();
