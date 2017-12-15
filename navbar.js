a778092449cfca0fed98569213278d7bc3af2ddb
lorenzobrusco <lorenzo.brusco@libero.it>
2017-12-15 16:43:23 +0100

    add row script

--- navbar.js
+++ navbar.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	
 	$('#search-input').focusin(function(){
 		$("#search-div").css("width","70%");
 	});
@@ -7,8 +8,5 @@ $(document).ready(function() {
 		$("#search-div").css("width","50%");
 	});
 	$('[data-toggle="tooltip"]').tooltip();
-	let progressBar = document.querySelector("#rainbow-progress-bar");
-	window.onload = function() {
-		progressBar.style.display = 'none';
-	};	
+	
 });
---
+++ progressBar.js
@@ -0,0 +1,4 @@
+let progressBar = document.querySelector("#rainbow-progress-bar");
+	window.onload = function() {
+		progressBar.style.display = 'none';
+	};	
