5f4c4f711b61660611de1991c2afde14536f17e1
lorenzobrusco <lorenzo.brusco@libero.it>
2017-12-15 16:13:56 +0100

    add progress bar and local icons

--- navbar.js
+++ navbar.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-  $('#search-input').focusin(function(){
-    $("#search-div").css("width","70%");
-  });
+	$('#search-input').focusin(function(){
+		$("#search-div").css("width","70%");
+	});
 
-  $('#search-input').focusout(function(){
-    $("#search-div").css("width","50%");
-  });
-  $('[data-toggle="tooltip"]').tooltip();
+	$('#search-input').focusout(function(){
+		$("#search-div").css("width","50%");
+	});
+	$('[data-toggle="tooltip"]').tooltip();
+	let progressBar = document.querySelector("#rainbow-progress-bar");
+	window.onload = function() {
+		progressBar.style.display = 'none';
+	};	
 });
