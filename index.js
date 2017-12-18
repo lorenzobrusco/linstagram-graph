a6ac8b57858382ffb779578511796de9e45dee84
Alessio Scarfone <alessioscarfone93@gmail.com>
2017-12-18 23:43:23 +0100

    start configuration of dropzone

---
+++ index.js
@@ -0,0 +1,106 @@
+$(document).ready(
+  function () {
+    //show comment event
+    $('.show-all-comments')
+      .click(
+        function (e) {
+          var target = $(e.target).find('span');
+          if (target.hasClass('show-comments')) {
+            $(e.target).html('<span class="hidden-comments"></span> Nascondi altri commenti');
+          } else {
+            $(e.target).html('<span class="show-comments"></span> Carica altri commenti');
+          }
+        });
+
+    //Active tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //DROPZONE 
+    dropzoneOptions = {
+      url: 'upload',
+      autoProcessQueue: false,
+      uploadMultiple: true,
+      parallelUploads: 4,
+      maxFiles: 4,
+      maxFilesize: 2, //MB
+      acceptedFiles: "image/*",
+      addRemoveLinks: true,
+      dictDefaultMessage: 'Drop yuor photos or videos here',
+      init: function () {
+        var myDropzone = this;
+
+        var submit_button = $('.btn-create');
+        submit_button.prop("disabled", true);
+        this.on("thumbnail", function (file) {
+          if (myDropzone.getAcceptedFiles().length > 0)
+            submit_button.prop("disabled", false);
+        });
+
+        this.on("removedfile", function (file) {
+          console.log(myDropzone.getAcceptedFiles());
+          if (myDropzone.getAcceptedFiles().length == 0)
+            submit_button.prop("disabled", true);
+        });
+
+        // First change the button to actually tell Dropzone to process the queue.
+        this.element
+          .querySelector("button[type=submit]")
+          .addEventListener("click", function (e) {
+            // Make sure that the form isn't actually being sent.
+            e.preventDefault();
+            e.stopPropagation();
+            myDropzone
+              .processQueue();
+
+            // Remove rejected files
+            var rejected_files = myDropzone.getRejectedFiles();
+            console.log(rejected_files);
+            for (var rejected_file of rejected_files) {
+              myDropzone.removeFile(rejected_file);
+            }
+
+          });
+
+        // Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
+        // of the sending event because uploadMultiple is set to true.
+        this.on("sendingmultiple", function () {
+          // Gets triggered when the form is actually being sent.
+          // Hide the success button or the complete form.
+        });
+        this.on("successmultiple", function (files,
+          response) {
+          // Gets triggered when the files have successfully been sent.
+          // Redirect user or notify of success.
+        });
+        this.on("errormultiple", function (files,
+          response) {
+          // Gets triggered when there was an error sending the files.
+          // Maybe show form again, and notify user of error
+        });
+      } //close init
+    }; //close option
+
+    var uploader = document
+      .querySelector('#post-dropzone');
+    //CREATE DROPZONE
+    var myDropzone = new Dropzone(uploader,
+      dropzoneOptions);
+
+    //Active modal
+    $("#open-create-post-modal").animatedModal({
+      modalTarget: 'create-post-modal',
+      animatedIn: 'lightSpeedIn',
+      animatedOut: 'bounceOutDown',
+      color: '#fafafa80',
+      // Callbacks
+      beforeOpen: function () {},
+      afterOpen: function () {},
+      beforeClose: function () {},
+      afterClose: function () {
+        $("#post-description-input").val("");
+        //clean dropzone uploads
+        myDropzone.removeAllFiles(true);
+      }
+    });
+
+  });
